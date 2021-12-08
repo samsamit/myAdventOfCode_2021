1d5aaeb621a39fa03588549a4470ef9db1c77fcf
Samu Tiainen <samu.tiainen@hotmail.com>
2021-12-08 18:38:57 +0200

    d8

--- d8/segment.py
+++ d8/segment.py
@@ -7,11 +7,11 @@ lines = file1.readlines()
 ins, outs = [], []
 for line in lines:
     sLine = line.strip()
-    S_inputs, S_outputs = sLine.split('|')
-    ins.append(S_inputs.split(' '))
-    outs.append(S_outputs.split(' '))
+    S_inputs, S_outputs = sLine.split("|")
+    ins.append(S_inputs.split(" "))
+    outs.append(S_outputs.split(" "))
 
-# # part 1 
+# # part 1
 # count = 0
 # for out in outs:
 #     for str in out:
@@ -23,14 +23,15 @@ for line in lines:
 # part 2
 
 display = {
-    "top": None,
-    "tLeft": None,
-    "tRight": None,
-    "middle": None,
-    "bLeft": None,
-    "bRight": None,
-    "bot": None
+    "top": "",
+    "tLeft": "",
+    "tRight": "",
+    "middle": "",
+    "bLeft": "",
+    "bRight": "",
+    "bot": "",
 }
+
 
 def extract(extFrom, extThease):
     ext = extFrom
@@ -38,71 +39,130 @@ def extract(extFrom, extThease):
         ext = ext.replace(char, "")
     return ext
 
+
 def strSort(str):
     sort_s = sorted(str)
     a_str = "".join(sort_s)
     return a_str
 
+
 def findNum(key, list):
     sKey = strSort(key)
+    res = []
     for i in range(len(list)):
         segment = strSort(list[i])
         if sKey == segment:
-            return i
-
-def findPromising(segments, target):
-    promising = ("", 99)
-    for segment in segments:
-        ext = extract(segment, target)
-        print(len(ext))
-        if len(ext) < promising[1]:
-            promising = (ext, len(ext))
-    return promising
+            res.append(i)
+    if len(res) > 1:
+        print("multifound")
+        print(res)
+    else:
+        return res[0]
 
 
+# ins = [ins[2]]
 
-ins = [ins[0]]
-
-for In in ins:
+results = []
+for i in range(len(ins)):
     numbers = [None for i in range(10)]
+    print(ins[i])
     segments = []
-    for i in In:
-        if len(i) == 2:
-            numbers[1] = i
-        elif len(i) == 3:
-            numbers[7] = i
-        elif len(i) == 4:
-            numbers[4] = i
-        elif len(i) == 7:
-            numbers[8] = i
+    for j in ins[i]:
+        if len(j) == 2:
+            numbers[1] = j
+        elif len(j) == 3:
+            numbers[7] = j
+        elif len(j) == 4:
+            numbers[4] = j
+        elif len(j) == 7:
+            numbers[8] = j
         else:
-            if len(i) > 0:
-                segments.append(i)
-        
+            if len(j) > 0:
+                segments.append(j)
+
     thisDisplay = display
 
-    print(numbers)
     # get top
     ext = extract(numbers[7], numbers[1])
     thisDisplay["top"] = ext
 
     # nro 3
+    removeIndex = None
+    for segmentI in range(len(segments)):
+        if len(segments[segmentI]) == 5:
+            ext = extract(segments[segmentI], numbers[1])
+            if len(ext) == 3:
+                numbers[3] = segments[segmentI]
+                removeIndex = segmentI
+                continue
+    del segments[removeIndex]
+
+    # nro. 6
+    removeIndex = None
+    for segmentI in range(len(segments)):
+        if len(segments[segmentI]) == 6:
+            ext = extract(segments[segmentI], numbers[7])
+            if len(ext) == 4:
+                numbers[6] = segments[segmentI]
+                removeIndex = segmentI
+                continue
+    del segments[removeIndex]
+
+    # nro. 0
+    removeIndex = None
+    for segmentI in range(len(segments)):
+        if len(segments[segmentI]) == 6:
+            ext = extract(segments[segmentI], numbers[3])
+            if len(ext) == 2:
+                numbers[0] = segments[segmentI]
+                removeIndex = segmentI
+                continue
+    del segments[removeIndex]
+
+    # bottom
+    ext = extract(numbers[3], numbers[7] + numbers[4])
+    thisDisplay["bot"] = ext
+
+    # bLeft
+    ext = extract(numbers[8], numbers[3] + numbers[4])
+    thisDisplay["bLeft"] = ext
+
+    # 9
+    foundIndx = findNum(extract(numbers[8], thisDisplay["bLeft"]), segments)
+    numbers[9] = segments[foundIndx]
+    del segments[foundIndx]
+
+    # nro 2, 5
     for segment in segments:
         if len(segment) == 5:
-            ext = extract(segment, numbers[1])
+            ext = extract(segment, numbers[9])
+            print(segment, ext, numbers[9])
+            if len(ext) == 1:
+                numbers[2] = segment
+            if len(ext) == 0:
+                numbers[5] = segment
+
+    # tleft
+    ext = extract(numbers[8], numbers[2] + numbers[1])
+    thisDisplay["tLeft"] = ext
+
+    # nro 2, 5
+    for segment in segments:
+        if len(segment) == 6:
+            ext = extract(segment, numbers[7])
+            print(segment, ext, numbers[9])
             if len(ext) == 3:
-                numbers[3] = ext
-
-    # # nro. 9
-    # fNine = extract(numbers[8], thisDisplay["bLeft"])
-    # nineIndex = findNum(fNine, segment)
-    # print(nineIndex)
-                
-
-
-
+                numbers[0] = segment
 
     print(numbers)
-    print(thisDisplay)
+    print(display)
+    output = ""
+    for out in outs[i]:
+        if len(out) > 0:
+            print(out, numbers)
+            number = findNum(out, numbers)
+            output += str(number)
+    results.append(int(output))
+    print(f"Found: {int(output)} at index {i}")
 
-
+print(f"result is: {sum(results)}")

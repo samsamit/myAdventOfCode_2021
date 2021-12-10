59b579c93c1d2ffe2ccd60615a48468a062ed50a
visma-samu-tiainen <samu.tiainen@visma.com>
2021-12-10 11:01:57 +0200

    d10 something wrong

---
+++ d10/syntax.py
@@ -0,0 +1,116 @@
+import math
+file1 = open("syntaxData.txt", "r")
+lines = file1.readlines()
+
+
+def fifi(char):
+    opens.append(char)
+
+
+def take():
+    opens.pop(len(opens)-1)
+
+
+def close(char):
+    opener = ""
+    if char == ')':
+        opener = '('
+    if char == '}':
+        opener = '{'
+    if char == ']':
+        opener = '['
+    if char == '>':
+        opener = '<'
+
+    if opens[len(opens)-1] == opener:
+        take()
+        return True
+    else:
+        print('Found ' + opener + 'wanted ' + opens[len(opens)-1])
+        return False
+
+
+lines = [
+    '[({(<(())[]>[[{[]{<()<>>',
+    '[(()[<>])]({[<{<<[]>>(',
+    '(((({<>}<{<{<>}{[]{[]{}',
+    '{<[[]]>}<{[{[{[]{()[[[]',
+    '<{([{{}}[<[[[<>{}]]]>[]]'
+]
+errors = []
+openers = []
+for line in lines:
+    sLine = line.strip()
+    opens = []
+    for char in sLine:
+        if char == "(":
+            fifi("(")
+        elif char == "[":
+            fifi('[')
+        elif char == "{":
+            fifi('{')
+        elif char == "<":
+            fifi('<')
+        else:
+            if not close(char):
+                errors.append(char)
+                break
+    openers.append(opens)
+
+# part 1
+# print(errors)
+# score = 0
+# for char in errors:
+#     if char == ')':
+#         score += 3
+#     if char == '}':
+#         score += 1197
+#     if char == ']':
+#         score += 57
+#     if char == '>':
+#         score += 25137
+# print(score)
+
+# part 2
+
+
+def getCloser(char):
+    if char == "(":
+        return ')'
+    elif char == "[":
+        return ']'
+    elif char == "{":
+        return '}'
+    elif char == "<":
+        return '>'
+
+
+closers = []
+for opener in openers:
+    opener.reverse()
+    closer = []
+    for char in opener:
+        closer.append(getCloser(char))
+    closers.append(closer)
+    print(closer)
+
+scores = []
+for closer in closers:
+    score2 = 0
+    for char in closer:
+        score2 = score2 * 5
+        if char == ')':
+            score2 += 1
+        if char == '}':
+            score2 += 3
+        if char == ']':
+            score2 += 2
+        if char == '>':
+            score2 += 4
+    scores.append(score2)
+
+scores.sort()
+print(scores)
+middleIdx = math.floor(len(scores) / 2)
+print(f'{middleIdx} / {len(scores)}')
+print(scores[middleIdx])

1d11cf552dee8b87850eab105b846c82076a1eac
visma-samu-tiainen <samu.tiainen@visma.com>
2021-12-17 09:37:12 +0200

    d14 done

--- d14/polymers.py
+++ d14/polymers.py
@@ -10,33 +10,32 @@ for line in lines:
 #     print(x)
 
 def addPairs(newPairs, templatePairs):
-    
     newTemplatePairs = [*templatePairs]
     for pair in newPairs:
         found = False
         for j in range(len(templatePairs)):
-            if templatePairs[j][0] == pair:
-                newTemplatePairs[j][1] += 1
+            if templatePairs[j][0] == pair[0]:
+                newTemplatePairs[j][1] += pair[1]
                 found = True
         if not found:
-            newTemplatePairs.append([pair, 1])
+            newTemplatePairs.append(pair)
     return newTemplatePairs
 
 template = 'OOVSKSPKPPPNNFFBCNOV'
 
-template = 'NNCB'
+# template = 'NNCB'
 
 templatePairs = []
 for i in range(len(template)-1):
     pair = template[i:i+2]
-    templatePairs = addPairs([pair], templatePairs)
+    templatePairs = addPairs([[pair, 1]], templatePairs)
 
 
 
 def polymerize(pair):
     for insert in insertions:
-        if insert[0] == pair:
-            return [pair[0] + insert[1], insert[1] + pair[1]]
+        if insert[0] == pair[0]:
+            return [[pair[0][0] + insert[1], pair[1]], [insert[1] + pair[0][1], pair[1]]]
     return None
 
 def remove(pairs, templatePairs):
@@ -53,35 +52,51 @@ def remove(pairs, templatePairs):
 def getPolymerString(templatePairs):
     polymerStr = ''
     for pair in templatePairs:
-        polymerStr += pair[0][0]
+        for i in range(pair[1]):
+            polymerStr += pair[0][0]
     polymerStr += templatePairs[len(templatePairs)-1][0][1]
     return polymerStr
 
-def countPolymerString(polStr):
+def getChars(polArr):
+    chars = []
+    polArr.append([polArr[len(polArr)-1][0][1] + 'X', 1])
+    for pair in polArr:
+        found = False
+        for i in range(len(chars)):
+            if chars[i][0] == pair[0][0]:
+                chars[i][1] += pair[1]
+                found = True
+                break
+        if not found:
+            chars.append([pair[0][0], pair[1]])
+    return chars
+
+def countPolymerString(polArr):
     high, low = 0, 0
-    
+    chars = getChars(polArr)
+    for char in chars:
+        if char[1] > high or high == 0:
+            high = char[1]
+            print('high == ' + char[0])
+        if char[1] < low or low == 0:
+            low = char[1]
+            print('low == ' + char[0])
+    return high, low
 
-
-for i in range(1):
+for i in range(40):
+    print(i)
     allNews = []
-    allDels = []
     for pair in templatePairs:
-        newPairs = polymerize(pair[0])
-        print(pair[0], newPairs)
+        newPairs = polymerize(pair)
         if newPairs is not None:
-            allNews.extend(newPairs)
-            allDels.append(pair[0])
+            allNews = addPairs(newPairs, allNews)
         else:
             print('pair ' + str(pair) + ' not found in insertion table')
-    print("allDels")
-    for x in allDels:
-        print(x)
+    templatePairs = allNews
 
-    templatePairs = remove(allDels, templatePairs)
-    templatePairs = addPairs(allNews, templatePairs)
-
-polStr = getPolymerString(templatePairs)
-print(polStr)
+print(templatePairs[len(templatePairs)-1])
+high, low = countPolymerString(templatePairs)
+print(f'High: {high} - Low: {low} - Total: {high-low +1}') # +1 probably because the last char in the polystring is not counted in my code...
 
 
 
